f2c95b2d46ed561873a1df3b223f03c0116eec08
Samreen <samreenreyaz@outlook.com>
2019-01-21 00:09:59 +0530

    Successful push and pull

--- app/src/main/java/com/noideastudios/hrms/AllApplicationsActivity.java
+++ app/src/main/java/com/noideastudios/hrms/AllApplicationsActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -11,22 +12,36 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class AllApplicationsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     DBhandler dBhandler;
-    Button sortBy;
+    Button sortBy, push, retrieve;
     RadioGroup radioGroup;
     RadioButton radioButton;
     ListView oldListView;
     ArrayList<Candidate> arrayList;
     CandidateAdapter candidateAdapter;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    ChildEventListener childEventListener;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +59,7 @@ public class AllApplicationsActivity extends AppCompatActivity implements Adapte
     private void setUpView() {
         dBhandler = new DBhandler(this, null, null, 1);
         oldListView = findViewById(R.id.oldCandidateList);
-        arrayList = dBhandler.returnCandidates(0);
-        candidateAdapter = new CandidateAdapter(
-                AllApplicationsActivity.this, R.layout.candidate_tile, arrayList);
-        oldListView.setAdapter(candidateAdapter);
-        oldListView.setOnItemClickListener(this);
+        getValues();
 
         Button delete = findViewById(R.id.delete);
         delete.setOnClickListener(new View.OnClickListener() {
@@ -66,6 +77,156 @@ public class AllApplicationsActivity extends AppCompatActivity implements Adapte
                 setUpSortBy();
             }
         });
+
+        push = findViewById(R.id.push);
+        push.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pushToRD();
+            }
+        });
+
+        retrieve = findViewById(R.id.retrieve);
+        retrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                childEventListener = null;
+                retrieveFromRD();
+            }
+        });
+    }
+
+    private void getValues() {
+        arrayList = dBhandler.returnCandidates(0);
+        candidateAdapter = new CandidateAdapter(
+                AllApplicationsActivity.this, R.layout.candidate_tile, arrayList);
+        oldListView.setAdapter(candidateAdapter);
+        oldListView.setOnItemClickListener(this);
+    }
+
+    private void retrieveFromRD() {
+        final View pullDialog = getLayoutInflater().inflate(R.layout.request_key, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(AllApplicationsActivity.this);
+        builder.setView(pullDialog);
+
+        final AlertDialog dialog = builder.create();
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        progressBar = pullDialog.findViewById(R.id.progress);
+        progressBar.setVisibility(View.GONE);
+
+        final Button key_proceed = pullDialog.findViewById(R.id.key_proceed);
+        key_proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                key_proceed.setClickable(false);
+                progressBar.setVisibility(View.VISIBLE);
+                TextInputEditText keyTIET = pullDialog.findViewById(R.id.Key);
+                final String key = keyTIET.getText().toString();
+                if (!key.isEmpty()) {
+                    firebaseDatabase = FirebaseDatabase.getInstance();
+                    databaseReference = firebaseDatabase.getReference();
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (!dataSnapshot.hasChild(key)) {
+                                key_proceed.setClickable(true);
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(AllApplicationsActivity.this, "Key does not exist!", Toast.LENGTH_LONG).show();
+                            } else {
+                                if (childEventListener == null) {
+                                    childEventListener = new ChildEventListener() {
+                                        @Override
+                                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                                            Candidate candidate = dataSnapshot.getValue(Candidate.class);
+                                            dBhandler.addCandidate(candidate);
+                                            dialog.dismiss();
+                                            getValues();
+                                            progressBar.setVisibility(View.GONE);
+                                            Toast.makeText(AllApplicationsActivity.this, "Retrieval Successful!", Toast.LENGTH_LONG).show();
+                                        }
+
+                                        @Override
+                                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                                        }
+
+                                        @Override
+                                        public void onChildRemoved(DataSnapshot dataSnapshot) {
+                                        }
+
+                                        @Override
+                                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+                                        }
+                                    };
+                                }
+                                databaseReference.child(key).addChildEventListener(childEventListener);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                    getValues();
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    key_proceed.setClickable(true);
+                    Toast.makeText(AllApplicationsActivity.this, "Key cannot be empty!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        progressBar.setVisibility(View.GONE);
+        key_proceed.setClickable(true);
+    }
+
+    private void pushToRD() {
+        final View pushDialog = getLayoutInflater().inflate(R.layout.request_key, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(AllApplicationsActivity.this);
+        builder.setView(pushDialog);
+
+        final AlertDialog dialog = builder.create();
+        Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+
+        progressBar = pushDialog.findViewById(R.id.progress);
+        progressBar.setVisibility(View.GONE);
+
+        final Button key_proceed = pushDialog.findViewById(R.id.key_proceed);
+        key_proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                key_proceed.setClickable(false);
+                TextInputEditText keyTIET = pushDialog.findViewById(R.id.Key);
+                String key = keyTIET.getText().toString();
+                if (!key.isEmpty()) {
+                    firebaseDatabase = FirebaseDatabase.getInstance();
+                    databaseReference = firebaseDatabase.getReference().child(key);
+                    if (childEventListener != null)
+                        databaseReference.removeEventListener(childEventListener);
+
+                    Map<String, Candidate> sample = new HashMap<>();
+                    for (Candidate entry : arrayList) {
+                        sample.put(String.valueOf(entry.getId()), entry);
+                    }
+                    databaseReference.setValue(sample);
+                    Toast.makeText(AllApplicationsActivity.this, "Push successful!", Toast.LENGTH_LONG).show();
+                    dialog.dismiss();
+                } else {
+                    progressBar.setVisibility(View.GONE);
+                    key_proceed.setClickable(true);
+                    Toast.makeText(AllApplicationsActivity.this, "Key cannot be empty!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        progressBar.setVisibility(View.GONE);
+        key_proceed.setClickable(true);
     }
 
     private void setUpSortBy() {
